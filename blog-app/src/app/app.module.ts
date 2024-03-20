7b99a8cc4fc2f5ea70a2c4493fb972f677c70115
Georgi <g_garenov@outlook.com>
2024-03-20 21:39:59 +0200

    post card template is created and designed

--- blog-app/src/app/app.module.ts
+++ blog-app/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { SubscriptionFormComponent } from './subscription-form/subscription-form
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
 import { AboutComponent } from './pages/about/about.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { AboutComponent } from './pages/about/about.component';
     CommentFormComponent,
     CommentListComponent,
     AboutComponent,
+    PostCardComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
