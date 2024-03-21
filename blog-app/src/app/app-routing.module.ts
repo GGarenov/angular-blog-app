888df475b313a72b58ecf53918fc7f4810cf1db7
Georgi <g_garenov@outlook.com>
2024-03-21 19:58:37 +0200

    Single post component is designed

--- blog-app/src/app/app-routing.module.ts
+++ blog-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -9,7 +10,7 @@ import { AboutComponent } from './pages/about/about.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'category', component: SingleCategoryComponent },
-  { path: 'post', component: SingleCategoryComponent },
+  { path: 'post', component: SinglePostComponent },
   { path: 'about', component: AboutComponent },
   { path: 'term-conditions', component: TermsAndConditionsComponent },
   { path: 'contact', component: ContactUsComponent },
