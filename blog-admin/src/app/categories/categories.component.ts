246b7973b95e499e1a06c1e6003cdafb9c2ccd2b
Georgi <g_garenov@outlook.com>
2024-03-24 19:25:26 +0200

    Categories and subcategories are created and linked to firebase, but will be optimized later.

--- blog-admin/src/app/categories/categories.component.ts
+++ blog-admin/src/app/categories/categories.component.ts
@@ -14,12 +14,39 @@ export class CategoriesComponent {
     let categoryData = {
       category: formData.value.category,
     };
+    let subCategoryData = {
+      subCategory: 'subCategory1',
+    };
 
     this.firestore
       .collection('categories')
       .add(categoryData)
       .then((docRef) => {
         console.log(docRef);
+        this.firestore
+          .collection('categories')
+          .doc(docRef.id)
+          .collection('subCategories')
+          .add(subCategoryData)
+          .then((docRef1) => {
+            console.log(docRef1);
+            this.firestore
+              .collection('categories')
+              .doc(docRef.id)
+              .collection('subCategories')
+              .doc(docRef1.id)
+              .collection('subSubCategories')
+              .add(subCategoryData)
+              .then((docRef2) => {
+                console.log('Second level sub category added');
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       })
       .catch((err) => {
         console.log(err);
