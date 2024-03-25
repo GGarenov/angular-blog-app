d82d2744ebfdd2cd87332672b019d047b320fd3f
Georgi <g_garenov@outlook.com>
2024-03-25 23:34:49 +0200

    I tried to fetch the data from firebase and to show it in the new table that I created, but unfortunately without any success..

--- blog-admin/src/app/categories/categories.component.ts
+++ blog-admin/src/app/categories/categories.component.ts
@@ -3,17 +3,25 @@ import { NgForm } from '@angular/forms';
 import { CategoriesService } from '../services/categories.service';
 import { Category } from '../models/category';
 
+interface CategoryData {
+  id: string;
+  data: Category;
+}
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
+  categoryArray: Array<CategoryData> = [];
+
   constructor(private categoryService: CategoriesService) {}
 
   ngOnInit(): void {
     this.categoryService.loadData().subscribe((val: any) => {
       console.log(val);
+      this.categoryArray = val;
     });
   }
 
