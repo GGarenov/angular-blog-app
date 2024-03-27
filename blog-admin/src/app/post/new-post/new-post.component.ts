62ec3a2932bddc1cbae83082356ec2768a8aa969
Georgi <g_garenov@outlook.com>
2024-03-28 00:16:41 +0200

    Replace javascript method is used for permalink so that the links to be with -

--- blog-admin/src/app/post/new-post/new-post.component.ts
+++ blog-admin/src/app/post/new-post/new-post.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+  styleUrls: ['./new-post.component.css'],
 })
 export class NewPostComponent {
-
+  onTitleChanged(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const title = target.value;
+    let permalink = title.replace(/\s/g, '-');
+    console.log(permalink);
+  }
 }
