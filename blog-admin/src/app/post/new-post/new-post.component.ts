e609bce73c6a1a290da605145e5ea1415046d88d
Georgi <g_garenov@outlook.com>
2024-03-30 19:57:30 +0200

    Little rearrangement in order to prevent from bugs

--- blog-admin/src/app/post/new-post/new-post.component.ts
+++ blog-admin/src/app/post/new-post/new-post.component.ts
@@ -40,29 +40,41 @@ export class NewPostComponent implements OnInit {
   ) {
     this.route.queryParams.subscribe((val) => {
       this.docId = val['id'];
-      this.postService.loadOneData(val['id']).subscribe((post) => {
-        this.post = post;
-        this.postForm = this.fb.group({
-          title: [
-            this.post.title,
-            [Validators.required, Validators.minLength(10)],
-          ],
-          permalink: [this.post.permalink, [Validators.required]],
-          excerpt: [
-            this.post.excerpt,
-            [Validators.required, Validators.minLength(10)],
-          ],
-          category: [
-            `${this.post.category.categoryId}-${this.post.category.category}`,
-            [Validators.required],
-          ],
-          postImg: ['', [Validators.required]],
-          content: [this.post.content, [Validators.required]],
-        });
 
-        this.imgSrc = this.post.postImgPath;
-        this.formStatus = 'Edit';
-      });
+      if (this.docId) {
+        this.postService.loadOneData(val['id']).subscribe((post) => {
+          this.post = post;
+          this.postForm = this.fb.group({
+            title: [
+              this.post.title,
+              [Validators.required, Validators.minLength(10)],
+            ],
+            permalink: [this.post.permalink, [Validators.required]],
+            excerpt: [
+              this.post.excerpt,
+              [Validators.required, Validators.minLength(10)],
+            ],
+            category: [
+              `${this.post.category.categoryId}-${this.post.category.category}`,
+              [Validators.required],
+            ],
+            postImg: ['', [Validators.required]],
+            content: [this.post.content, [Validators.required]],
+          });
+
+          this.imgSrc = this.post.postImgPath;
+          this.formStatus = 'Edit';
+        });
+      } else {
+        this.postForm = this.fb.group({
+          title: ['', [Validators.required, Validators.minLength(10)]],
+          permalink: ['', [Validators.required]],
+          excerpt: ['', [Validators.required, Validators.minLength(10)]],
+          category: ['', [Validators.required]],
+          postImg: ['', [Validators.required]],
+          content: ['', [Validators.required]],
+        });
+      }
     });
   }
 
