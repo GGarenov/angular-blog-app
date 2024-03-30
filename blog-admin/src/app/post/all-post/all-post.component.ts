93ae435eb96d472442644b27681ec9a9a25cf673
Georgi <g_garenov@outlook.com>
2024-03-30 16:56:52 +0200

    Columns are created in the html and the data is passed in the rows

--- blog-admin/src/app/post/all-post/all-post.component.ts
+++ blog-admin/src/app/post/all-post/all-post.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from 'src/app/services/posts.service';
 
+interface Post {
+  postImgPath: string;
+  title: string;
+  excerpt: string;
+  category: { category: string };
+  createdAt: { seconds: number };
+  // Add other properties as needed
+}
+
 @Component({
   selector: 'app-all-post',
   templateUrl: './all-post.component.html',
   styleUrls: ['./all-post.component.css'],
 })
-export class AllPostComponent {
+export class AllPostComponent implements OnInit {
+  postArray: Post[] = [];
+
   constructor(private postService: PostsService) {}
 
   ngOnInit(): void {
-    this.postService.loadData().subscribe((val) => {
+    this.postService.loadData().subscribe((val: any) => {
       console.log(val);
+      this.postArray = val as Post[];
     });
   }
 }
