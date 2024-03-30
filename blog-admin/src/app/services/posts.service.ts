6200ca60ecee55ab7bb000bab9ce9853ac76be4e
Georgi <g_garenov@outlook.com>
2024-03-30 14:30:53 +0200

    Not everything working...

--- blog-admin/src/app/services/posts.service.ts
+++ blog-admin/src/app/services/posts.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostsService {
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private firestore: AngularFirestore,
+    private toastr: ToastrService
+  ) {}
 
   uploadImage(selectedImage: any, postData: string) {
     const filePath = `postIMG/${Date.now()}`;
@@ -22,6 +28,13 @@ export class PostsService {
           const postData = { postImgPath: '' }; // Define postData object with postImgPath property
           postData.postImgPath = URL;
           console.log(postData);
+
+          this.firestore
+            .collection('posts')
+            .add(postData)
+            .then((docRef) => {
+              this.toastr.success('Data saved successfully');
+            });
         });
     });
   }
