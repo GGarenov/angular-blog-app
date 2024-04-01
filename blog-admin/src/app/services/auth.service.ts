03ab0f776584080cb13dce411a3f6b633fee2bbf
Georgi <g_garenov@outlook.com>
2024-04-01 22:12:50 +0300

    The login logic is implemented in the service and in the component

--- blog-admin/src/app/services/auth.service.ts
+++ blog-admin/src/app/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(
+    private firebaseAuth: AngularFireAuth,
+    private toastr: ToastrService
+  ) {}
 
-  constructor() { }
+  login(email: string, password: string) {
+    return this.firebaseAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((logRef) => {
+        this.toastr.success('Login Successful');
+      });
+  }
 }
