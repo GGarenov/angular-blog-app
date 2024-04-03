d9a8b5c1de9bbc6b6fe403c8c6d50c9a7d84de4e
Georgi <g_garenov@outlook.com>
2024-04-03 19:04:23 +0300

    Toastr and router were added in order to show some message and not to redirect to a blank page.

--- blog-admin/src/app/services/auth.guard.ts
+++ blog-admin/src/app/services/auth.guard.ts
@@ -4,15 +4,21 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   UrlTree,
+  Router,
 } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -28,6 +34,8 @@ export class AuthGuard implements CanActivate {
       return true;
     } else {
       console.log('Access denied');
+      this.toastr.warning('Please login to access this page');
+      this.router.navigate(['/login']);
       return false;
     }
   }
