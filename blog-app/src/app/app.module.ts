b53aaef5b7aaa17aad09761192eb5d24554c89e8
Georgi <g_garenov@outlook.com>
2024-04-04 19:11:27 +0300

    Firebase is added in the project but there is a bug and needs to be fixed..

--- blog-app/src/app/app.module.ts
+++ blog-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import Angula
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
