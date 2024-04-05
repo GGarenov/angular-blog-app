c980cafe9c626f59fa53e1c65f71d260c90f7c04
Georgi <g_garenov@outlook.com>
2024-04-05 19:43:36 +0300

    Load data is implemented in the services and posts from firebase are loaded

--- blog-app/src/app/services/post.service.ts
+++ blog-app/src/app/services/post.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  loadData() {
+    return this.firestore
+      .collection('posts')
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data() as object;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          });
+        })
+      );
+  }
 }
