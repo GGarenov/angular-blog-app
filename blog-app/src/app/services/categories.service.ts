b59d476e44004d04d9669513896e82d7e56dd9db
Georgi <g_garenov@outlook.com>
2024-04-05 19:31:48 +0300

    Data from category is passed successfully

--- blog-app/src/app/services/categories.service.ts
+++ blog-app/src/app/services/categories.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
+
+interface Category {
+  id: string;
+  category: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoriesService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  loadData() {
+    return this.firestore
+      .collection('categories')
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const { id, ...data } = a.payload.doc.data() as Category; // Destructure id from data
+            const docId = a.payload.doc.id;
+            return { id: docId, ...data };
+          });
+        })
+      );
+  }
 }
