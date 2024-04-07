6e46c5de7fe980f14486e70c0149f8d3aec1a60f
Georgi <g_garenov@outlook.com>
2024-04-07 20:43:04 +0300

    Logic is implemented in the html and in the component successfully

--- blog-admin/src/app/subscribers/subscribers.component.ts
+++ blog-admin/src/app/subscribers/subscribers.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SubscribersService } from '../services/subscribers.service';
 
 @Component({
   selector: 'app-subscribers',
   templateUrl: './subscribers.component.html',
-  styleUrls: ['./subscribers.component.css']
+  styleUrls: ['./subscribers.component.css'],
 })
-export class SubscribersComponent {
+export class SubscribersComponent implements OnInit {
+  subscribersArray: Array<any> = [];
 
+  constructor(private subService: SubscribersService) {}
+
+  ngOnInit(): void {
+    this.subService.loadData().subscribe((val) => {
+      this.subscribersArray = val;
+      console.log(this.subscribersArray);
+    });
+  }
 }
