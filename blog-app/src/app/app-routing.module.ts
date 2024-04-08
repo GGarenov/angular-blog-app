95b182ffea02700b700e1f117088da47ed6d8235
Georgi <g_garenov@outlook.com>
2024-04-09 00:43:24 +0300

    The new pages are implemented in the router.

--- blog-app/src/app/app-routing.module.ts
+++ blog-app/src/app/app-routing.module.ts
@@ -8,18 +8,34 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AuthGuard } from './services/guard.service';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'category/:category/:id', component: SingleCategoryComponent },
-  { path: 'post/:id', component: SinglePostComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'term-conditions', component: TermsAndConditionsComponent },
-  { path: 'contact', component: ContactUsComponent },
+  {
+    path: 'category/:category/:id',
+    component: SingleCategoryComponent,
+  },
+  {
+    path: 'post/:id',
+    component: SinglePostComponent,
+  },
+  { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
+  {
+    path: 'term-conditions',
+    component: TermsAndConditionsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'contact',
+    component: ContactUsComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
